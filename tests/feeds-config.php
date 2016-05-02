e0726f5cb4ce05028f9815ce4083d63d9cc9e42d
Mykola Martynov <mykola.martynov@hotmail.com>
2016-05-02 17:34:15 +0300

    add feeds api package

---
+++ tests/feeds-config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'secure' => false,
+    'apiHost' => 'db-manager.promocodes2015.com',
+    'apiUrl' => '/feeds/api/1.0',
+    'apiKey' => 'APIKEY',
+    'secretKey' => 'SECRETKEY',
+);
--- tests/test.php
+++ tests/test.php
@@ -3,5 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use USPC\Feeds\FeedsConnector;
+use USPC\Feeds\ServiceConnection;
 
+$sc = new ServiceConnection(__DIR__ . '/feeds-config.php');
 $fc = new FeedsConnector();
