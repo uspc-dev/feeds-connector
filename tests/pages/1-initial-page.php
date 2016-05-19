28076838ed4965b8584bba289e1ca889613fd8ea
Mykola Martynov <mykola.martynov@hotmail.com>
2016-05-19 20:54:19 +0300

    add container for the initial page test

--- tests/pages/1-initial-page.php
+++ tests/pages/1-initial-page.php
@@ -14,7 +14,9 @@ $html = $fc->process();
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
-    <?= $html ?>
+    <div class="container">
+        <?= $html ?>
+    </div>
     <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>    
 </body>
